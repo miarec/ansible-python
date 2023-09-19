eeb597cff16e8659a7621e7d5c99ffb4c4e7419e
Timothy Lytle <timothy.lytle@miarec.com>
2023-09-19 13:25:13 -0600

    update molecule and github action to test different install methods

--- molecule/default/tests/test_defatuls.py
+++ molecule/default/tests/test_defatuls.py
@@ -5,11 +5,20 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 python_version = os.environ.get('PYTHON_VERSION')
+python_install_from_source = os.environ.get('PYTHON_INSTALL_FROM_SOURCE')
 
 def test_files(host):
-    files = [
-        "/usr/local/bin/python{}".format(".".join(python_version.split('.')[:2]))
-    ]
+
+    if python_install_from_source :
+        files = [
+            "/usr/bin/python3"
+        ]
+    else :
+        files = [
+            "/usr/local/bin/python{}".format(".".join(python_version.split('.')[:2]))
+        ]
+
+
     for file in files:
         f = host.file(file)
         assert f.exists
