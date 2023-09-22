8afffd7cc0972cd05970d29d5740d025987be1f3
timothylytle <87677364+timothylytle@users.noreply.github.com>
2023-09-22 15:25:24 -0600

    Add support for Python versions 3.7-3.12
    + Add moledule testing and Github Actions
    + Add additional dependencies to allow the module to be deployed by itself

---
+++ molecule/default/tests/test_defatuls.py
@@ -0,0 +1,18 @@
+import os
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+python_version = os.environ.get('PYTHON_VERSION')
+
+
+
+def test_files(host):
+    files = [
+        "/usr/local/bin/python{}".format(".".join(python_version.split('.')[:2]))
+    ]
+    for file in files:
+        f = host.file(file)
+        assert f.exists
+        assert f.is_file
